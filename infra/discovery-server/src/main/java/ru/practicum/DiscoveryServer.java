0a942e8d4809839b3f585a6cc55306d47bf91700
xSeMx <xsemx@yandex.ru>
2025-03-14 20:43:55 +0400

    spring-cloud

--- infra/discovery-server/src/main/java/ru/practicum/DiscoveryServer.java
+++ infra/discovery-server/src/main/java/ru/practicum/DiscoveryServer.java
@@ -1,4 +1,4 @@
-package ru.practicum.discoveryserver;
+package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class DiscoveryServerApplication {
+public class DiscoveryServer {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiscoveryServerApplication.class, args);
+        SpringApplication.run(DiscoveryServer.class, args);
     }
 
 }
