6314a845618a64553b0308436a26841ac6064dc8
xSeMx <xsemx@yandex.ru>
2025-03-20 07:55:28 +0400

    spring-cloud

--- stats-service/service/src/main/java/ru/practicum/StatsServerApp.java
+++ stats-service/service/src/main/java/ru/practicum/StatsServerApp.java
@@ -2,8 +2,9 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class StatsServerApp {
     public static void main(String[] args) {
