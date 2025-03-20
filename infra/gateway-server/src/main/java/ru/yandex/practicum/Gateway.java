8d6f4ec6caba9d6d404040975f8c2771443b2a3b
xSeMx <xsemx@yandex.ru>
2025-03-20 07:40:55 +0400

    spring-cloud

--- infra/gateway-server/src/main/java/ru/yandex/practicum/Gateway.java
+++ infra/gateway-server/src/main/java/ru/yandex/practicum/Gateway.java
@@ -2,7 +2,9 @@ package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Gateway {
     public static void main(String[] args) {
